626eb36cf3c47f814b05f30d144c0f8f62c5a9e7
Matheus Trojan <matheustrojan23@gmail.com>
2022-09-09 10:30:29 -0300

    Iniciado lista de filmes (basico)

--- alura-flix/main.js
+++ alura-flix/main.js
@@ -1,7 +1,9 @@
-var filme1 = "Interestelar"
-var filme2 = "Lord of the Rings - Return of the King"
-var filme3 = "Star Wars - A New Hope"
+var listaFilmes = ["https://play-lh.googleusercontent.com/em2griqrHoxmxEss_WM5Fi2iqSEKrEfLNAltjX54lREOR0nz0du__KuSi2bA_YNjS4w", "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/d20b35b9c0734c429b652bd0280158a8c7c0dfa1a4c2254d0312eacf4c902c32._RI_V_TTW_.jpg", "https://kbimages1-a.akamaihd.net/538b1473-6d45-47f4-b16e-32a0a6ba7f9a/1200/1200/False/star-wars-episode-iv-a-new-hope-3.jpg"]
 
-document.write("<h1 class='page-title'>" + filme1 + "</h1>");
-document.write("<h1 class='page-title'>" + filme2 + "</h1>");
-document.write("<h1 class='page-title'>" + filme3 + "</h1>");
+// push adiciona items na lista
+listaFilmes.push("https://images-na.ssl-images-amazon.com/images/S/pv-target-images/cf612039123df81f21a44121b8a229f1f9088947b7a4bebeae4ee2467fa82ad1._RI_V_TTW_.jpg");
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+};
+
