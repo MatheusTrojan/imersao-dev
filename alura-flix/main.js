fe097f4f3e1cc8188dfd68b83c3685504c0c0473
Matheus Trojan <matheustrojan23@gmail.com>
2022-09-09 09:45:19 -0300

    Ajustado textos

---
+++ alura-flix/main.js
@@ -0,0 +1,7 @@
+var filme1 = "Interestelar"
+var filme2 = "Lord of the Rings - Return of the King"
+var filme3 = "Star Wars - A New Hope"
+
+document.write("<h1 class='page-title'>" + filme1 + "</h1>");
+document.write("<h1 class='page-title'>" + filme2 + "</h1>");
+document.write("<h1 class='page-title'>" + filme3 + "</h1>");
