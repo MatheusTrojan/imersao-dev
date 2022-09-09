738a2cb4b6e7753f4a7db8420b5e719b50df1f97
Matheus Trojan <matheustrojan23@gmail.com>
2022-09-09 18:34:20 -0300

    Organizei o código
    Falta colocar botão de remover, verificação de titulos duplicados

--- alura-flix/main.js
+++ alura-flix/main.js
@@ -1,25 +1,29 @@
 function adicionarFilme() {
     // variavel para pegar o value do input com id "filme"
-    var filmeFavorito = document.getElementById("filme").value;
     var tituloFilme = document.getElementById("titulo__filme").value;
-    // verificação de se o link inserido termina com .jpg
-    if (filmeFavorito.endsWith(".jpg")) {
-        listarFilmesNaTela(filmeFavorito, tituloFilme);
-    } else {
-        console.error("Endereço de filme inválido")
-    }
-    // apos salvar o filme, esse comando limpa o input 
-    document.getElementById("filme").value = "";
-    document.getElementById("titulo__filme").value = "";
+    var posterFilme = document.getElementById("poster__filme").value;
+
+    // verificação de se o link inserido termina com .jpg .png ou .jpeg
+        if (posterFilme.endsWith(".jpg") || posterFilme.endsWith(".png") || posterFilme.endsWith(".jpeg")) {
+            listarFilmesNaTela(posterFilme, tituloFilme);
+            alert("Filme adicionado com sucesso!")
+        } else {
+            console.error("Endereço de filme inválido")
+        } 
+
+    // apos salvar o filme, esse comando limpa o input
+    document.getElementById("titulo__filme").value = ""; 
+    document.getElementById("poster__filme").value = "";
+
 }
         // dentro do parenteses, pode ser qualquer nome, desde que tudo o que estiver sendo executado dentro da função com ele, referencie à variável que estamos recebendo
 function listarFilmesNaTela (imagem, titulo) {
     // variavel para pegar o que estiver escrito no input e colocar na biblioteca
-    var elementoFilmeFavorito = "<section class = 'filmes'><img src=" + imagem + ">" + "<h2 class = 'page-subtitle'>" + titulo + "</h2></section>";
+    var elementoFilmeFavorito = "<li class = 'filmes'><img src=" + imagem + ">" + "<h2 class = 'page-subtitle'>" + titulo + "</h2></li>";
 
-    // variavel para guardar a div com id listaFilmes
+    // variavel para guardar a lista (ul) com id listaFilmes
     var elementoListaFilmes = document.getElementById("listaFilmes");
 
-    // jogando o filme desejado dentro da div lista filme, já como img e sem sobrescrever o filme colocado anteriormente
+    // jogando o filme desejado dentro da ul listaFilmes, já com li > img e h2 < sem sobrescrever o filme colocado anteriormente
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito; 
 }
